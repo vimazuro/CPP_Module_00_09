c6b1b171f72b29ea063362b5c8e4c7627105186a
Victoria Mazurova <vimazuro@car13s1.42barcelona.com>
2025-12-29 14:58:06 +0100

    Correction ex00

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vimazuro <vimazuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 11:50:29 by vimazuro          #+#    #+#             */
-/*   Updated: 2025/09/12 12:42:53 by vimazuro         ###   ########.fr       */
+/*   Updated: 2025/12/29 14:52:48 by vimazuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 int	main()
 {
 	Zombie*	heap_zombie = newZombie("I_am_heap");
+	if (!heap_zombie)
+	{
+		std::cerr << "Error: memory allocation failed" << std::endl;
+		return 1;
+	}
 	heap_zombie->announce();
 	delete heap_zombie;
-
 	randomChump("I_am_stack");
-
 	return 0;
 }
