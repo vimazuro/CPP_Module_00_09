1d43cc91c3928a483334f54bf6d25a4594d1d055
Victoria Mazurova <vimazuro@car14s3.42barcelona.com>
2026-02-09 15:03:59 +0100

    CPP02/ex02 correction

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vimazuro <vimazuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:53:25 by vimazuro          #+#    #+#             */
-/*   Updated: 2026/02/04 19:11:01 by vimazuro         ###   ########.fr       */
+/*   Updated: 2026/02/09 14:58:03 by vimazuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,32 +28,3 @@ int main(void)
 
 	return 0;
 }
-
-/*	Fixed a(5.5f);
-	Fixed b(7.0f);
-	Fixed d(0.0f);
-	Fixed c = a / b;
-	
-	if (a > b)
-    	std::cout << "a is bigger than b" << std::endl;
-	if (a < b)
-		std::cout << "a is smaller than b" << std::endl;
-	if (a == b)
-		std::cout << "a is equal to b" << std::endl;
-	if (a != b)
-		std::cout << "a is not equal to b" << std::endl;
-	if (a >= b)
-		std::cout << "a is bigger than or equal to b" << std::endl;
-	if (a <= b)
-		std::cout << "a is smaller than or equal to b" << std::endl;
-	
-	std::cout << a << " / " << b << " equal to " << c << std::endl;
-	
-	std::cout << "--d = " << --d << std::endl;
-	std::cout << "++d = " << ++d << " ++d = " << ++d << std::endl;
-	std::cout << "d-- = " << d-- << "; d = " << d << std::endl;
-	std::cout << "d++ = " << d++ << "; d = " << d << std::endl;
-
-	return 0;
-}
-*/
