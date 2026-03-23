440cb726adefb0f591204dce9cbf0829eede7703
Victoria Mazurova <vimazuro@car13s1.42barcelona.com>
2026-03-23 19:08:33 +0100

    new main

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vimazuro <vimazuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 14:42:59 by vimazuro          #+#    #+#             */
-/*   Updated: 2026/03/19 15:38:34 by vimazuro         ###   ########.fr       */
+/*   Updated: 2026/03/23 18:31:52 by vimazuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,6 @@ int main()
 	a.takeDamage(8);
 	a.beRepaired(3);
 
-	for (int i = 0; i < 10; i++)
-		a.attack("Enemy");
-
-	a.takeDamage(5);
-	a.beRepaired(2);
 	a.guardGate();
 
 	return 0;
--- CPP03/ex02/main.cpp
+++ CPP03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vimazuro <vimazuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 14:42:59 by vimazuro          #+#    #+#             */
-/*   Updated: 2026/03/19 16:56:53 by vimazuro         ###   ########.fr       */
+/*   Updated: 2026/03/23 18:56:52 by vimazuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,6 @@ int main()
 	a.attack("Enemy");
 	a.takeDamage(8);
 	a.beRepaired(3);
-
-	for (int i = 0; i < 5; i++)
-		a.attack("Enemy");
-
-	a.takeDamage(5);
-	a.beRepaired(2);
 	a.highFivesGuys();
 
 	return 0;
